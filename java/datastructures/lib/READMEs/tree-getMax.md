662eac0803078f2107449947d654b424df793606
Stephen <stephenlevesque2@gmail.com>
2023-05-09 18:28:39 -0700

    lab16

---
+++ java/datastructures/lib/READMEs/tree-getMax.md
@@ -0,0 +1,17 @@
+# Binary Tree getMax()
+
+## Features:
+- Write the following method for the Binary Tree class
+
+  - find maximum value
+  - Arguments: none
+  - Returns: number
+- Find the maximum value stored in the tree. You can assume that the values stored in the Binary Tree will be numeric.
+
+
+## Whiteboard
+![cc16.png](..%2Fimg%2Fcc16.png)
+
+## Links to code:
+- [Binary Tree](..%2Fsrc%2Fmain%2Fjava%2Fdatastructures%2Ftree%2FBinaryTree.java)
+- [BinaryTreeTest](..%2Fsrc%2Ftest%2Fjava%2Fdatastructures%2Ftree%2FBinaryTreeTest.java)
--- java/datastructures/lib/src/main/java/datastructures/tree/BinarySearchTree.java
+++ java/datastructures/lib/src/main/java/datastructures/tree/BinarySearchTree.java
@@ -2,42 +2,50 @@ package src.main.java.datastructures.tree;
 
 import src.main.java.datastructures.tree.Node;
 
-public class BinarySearchTree extends BinaryTree {
-  public BinarySearchTree() {
-    super();
+public class BinarySearchTree<T extends Comparable<T>> extends BinaryTree<T> {
+
+  public BinarySearchTree(T value) {
+    super(value);
   }
 
-  public void add(int value) {
-    root = addHelper(root, value);
+  public void add(T value) {
+    this.root = add(this.root, value);
   }
 
-  private Node addHelper(Node node, int value) {
+  protected Node<T> add(Node<T> node, T value) {
     if (node == null) {
-      node = new Node(value);
-      return node;
+      return new Node<>(value);
     }
 
-    if (value < node.value) {
-      node.left = addHelper(node.left, value);
-    } else if (value > node.value) {
-      node.right = addHelper(node.right, value);
+    int cmp = value.compareTo(node.value);
+    if (cmp < 0) {
+      node.left = add(node.left, value);
+    } else if (cmp > 0) {
+      node.right = add(node.right, value);
+    } else {
+      // Value already exists in the tree; ignore the new value.
     }
 
     return node;
   }
 
-  public boolean contains(int value) {
-    return containsHelper(root, value);
+  public boolean contains(T value) {
+    return contains(this.root, value);
   }
 
-  private boolean containsHelper(Node node, int value) {
-    if (node == null) return false;
+  private boolean contains(Node<T> node, T value) {
+    if (node == null) {
+      return false;
+    }
 
-    if (value == node.value) return true;
-
-    if (value < node.value) {
-      return containsHelper(node.left, value);
+    int comparable = value.compareTo(node.value);
+    if (comparable < 0) {
+      return contains(node.left, value);
+    } else if (comparable > 0) {
+      return contains(node.right, value);
     } else {
-      return containsHelper(node.right, value);
+      return true;
     }
   }
+}
+
